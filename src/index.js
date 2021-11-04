81d3f54c055f98af86343958f4608841c52fb652
Nicholas W.Mazzei <nicholaswmazzei@outlook.com>
2021-11-04 19:04:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.post('/users', (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const {user} = request
-  return response.status(201).send(user?.todos)
+  return response.status(201).send(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
